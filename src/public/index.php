47978733885b23f90fec5b43927728132d4cbbef
andreasschollenberger <andreasschollenberger@gmail.com>
2024-02-12 16:13:12 +0100

    wiki button js 1.0

--- src/public/index.php
+++ src/public/index.php
@@ -64,6 +64,8 @@ include "./includes/data-collector.php"; // Muss zuerst sein wegen Start _SESSIO
             </form> 
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+    
 </body>
 
 </html>
