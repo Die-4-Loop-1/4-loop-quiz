9cf4991a098d14370ef4470af028ab3af96b4bce
mlavin7 <mlavin7@gmail.com>
2024-02-14 14:32:24 +0100

    styles css changes

--- src/public/quest.php
+++ src/public/quest.php
@@ -12,8 +12,8 @@
 
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css" />
-    <link rel="stylesheet" href="assets/css/q-annim.css" />
+    <link rel="stylesheet" href="./assets/css/style.css" />
+    <link rel="stylesheet" href="/.assets/css/q-annim.css" />
     <script src="assets/js/main.js"></script>
 
 
